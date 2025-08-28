5e529ec1609f4bc840354ef84d25f22b93d721f5
arthurveroch <verhofstadtpro@gmail.com>
2025-08-28 15:17:09 +0200

    Update email notification text in message lifecycle to clarify message context

--- src/api/message/content-types/message/lifecycles.js
+++ src/api/message/content-types/message/lifecycles.js
@@ -4,7 +4,7 @@ module.exports = {
       await strapi.plugin("email").service("email").send({
         to: "emeraude.nautisme@gmx.fr",
         subject: "Nouveau message",
-        text: "On vous a envoyé un message ! Vérifiez votre espace administrateur.",
+        text: "On vous a envoyé un message sur votre site ! Vérifiez votre espace administrateur.",
       });
     } catch (err) {
       strapi.log.error("Erreur envoi email (message.afterCreate):", err);
